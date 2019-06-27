e6bb7c2182a8c5adc6452b81fae9b8abca764ae6
Mohammad Safari <36362683+Mohammadsafarisouri@users.noreply.github.com>
2019-06-28 03:01:49 +0430

    GPIO

---
+++ GPIO/main.c
@@ -0,0 +1,25 @@
+#include "stm32f10x.h"                  // Device header
+
+
+int main(void) {
+	uint32_t i = 0;
+	
+	// Active Clock of Port C
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	
+	// GPIO_CRH_MODE13	--> Pin13 Output mode, max speed 50MHz
+	// GPIO_CRH_CNF13_0	--> Pin13 General purpose output Open-drain
+	GPIOC->CRH |= (GPIO_CRH_MODE13|GPIO_CRH_CNF13_0);
+	
+
+	
+	while(1) {
+		GPIOC->BSRR |= GPIO_BSRR_BR13;		//Bit 13 is Reset
+		for(i = 0;i<7200000;i++);
+		
+		GPIOC->BSRR |= GPIO_BSRR_BS13;		//Bit 13 is Set
+		for(i = 0;i<7200000;i++);
+		
+	}
+}
+
